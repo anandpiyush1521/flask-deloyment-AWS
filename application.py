9cac26d3ca8242d0ec7b34302fc3771db1b02a74
Piyush Anand <piyushanand2580@gmail.com>
2022-07-19 04:52:59 +0530

    small change to string

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "<h1>hello world</h1>"
+    return "<h1>hello world! Flask Deployment is completed</h1>"
 
 
 if __name__ == "__main__":
